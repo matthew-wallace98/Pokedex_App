bc984e2b2eb7f91ccc8dba514dce0d1efe11eca9
matthew-wallace98 <mattnicwall98@outlook.com>
2023-09-23 12:40:08 +0900

    Updated Pokemon List
    Added a loop, conditional, and ability to write the output of biggest Pokémon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 7,
-      types: ["grass", "poison"]
-    },
-    {
-      name: "Charmander",
-      height: 6,
-      types: ["fire"]
-    },
-    {
-      name: "Squirtle",
-      height: 5,
-      types: ["water"]
-    }
-  ];
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: "Charizard",
+    height: 1.7,
+    types: ["fire", "flying"]
+  },
+  {
+    name: "Squirtle",
+    height: 1,
+    types: ["water"]
+  }
+];
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemon = pokemonList[i];
+  let output = pokemon.name + " (height: " + pokemon.height + ")";
+  
+  // Add a condition to check the height
+  if (pokemon.height > 1.0) {
+      output += " - Wow, that's big!";
+  }
+  //.test
+  // Write the output
+  document.write(output + "<br>");
+}
